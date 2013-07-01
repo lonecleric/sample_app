e5a2bdc077ed2305a05413e0b49bbb4e72c48f9d
Gilles Duchesne <lonecleric@gmail.com>
2013-07-01 02:00:52 -0400

    Finish Chapter 8 Exercises

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -36,17 +36,12 @@ describe "User pages" do
         it { should have_selector('title', text: 'Sign up') }
         it { should have_content('error') }
         it { should have_content("can't be blank") }
-        it { should have_selector('div.alert.alert-error', text: 'error') }
+        it { should have_error_alert('error') }
       end
     end
 
     describe "with valid information" do
-      before do
-        fill_in "Name",         with: "Example User"
-        fill_in "Email",        with: "user@example.com"
-        fill_in "Password",     with: "foobar"
-        fill_in "Confirmation", with: "foobar"
-      end
+      before { fill_default_user }
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
@@ -57,7 +52,7 @@ describe "User pages" do
         let(:user) { User.find_by_email('user@example.com') }
 
         it { should have_selector('title', text: user.name) }
-        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+        it { should have_success_alert('Welcome') }
         it { should have_link('Sign out') }
       end
     end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,1 +1,26 @@
 include ApplicationHelper
+
+RSpec::Matchers.define :have_error_alert do |message|
+  match do |page|
+    page.should have_selector('div.alert.alert-error', text: message)
+  end
+end
+
+RSpec::Matchers.define :have_success_alert do |message|
+  match do |page|
+    page.should have_selector('div.alert.alert-success', text: message)
+  end
+end
+
+def valid_signin(user)
+  fill_in "Email",    with: user.email.upcase
+  fill_in "Password", with: user.password
+  click_button "Sign in"
+end
+
+def fill_default_user()
+  fill_in "Name",         with: "Example User"
+  fill_in "Email",        with: "user@example.com"
+  fill_in "Password",     with: "foobar"
+  fill_in "Confirmation", with: "foobar"
+end
