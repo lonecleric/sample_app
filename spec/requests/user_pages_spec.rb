1b3ee9a81b5e0a09587a18656a1fe263d50d157e
Gilles Duchesne <lonecleric@gmail.com>
2013-07-01 00:35:51 -0400

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -36,6 +36,7 @@ describe "User pages" do
         it { should have_selector('title', text: 'Sign up') }
         it { should have_content('error') }
         it { should have_content("can't be blank") }
+        it { should have_selector('div.alert.alert-error', text: 'error') }
       end
     end
 
@@ -57,6 +58,7 @@ describe "User pages" do
 
         it { should have_selector('title', text: user.name) }
         it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+        it { should have_link('Sign out') }
       end
     end
   end
