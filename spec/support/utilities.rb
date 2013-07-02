a038ac9c00f01732b87ec8b580ccf86a931de0d5
Gilles Duchesne <lonecleric@gmail.com>
2013-07-02 02:28:10 -0400

    Finish user edit, update, index, and destroy actions

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -13,9 +13,12 @@ RSpec::Matchers.define :have_success_alert do |message|
 end
 
 def valid_signin(user)
+  visit signin_path
   fill_in "Email",    with: user.email.upcase
   fill_in "Password", with: user.password
   click_button "Sign in"
+  # Sign in when not using Capybara as well.
+  cookies[:remember_token] = user.remember_token
 end
 
 def fill_default_user()
